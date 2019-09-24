90a7084310a28ce9d6de7c669bf81101dade1b3c
Anne Fausto <a.r.fausto@gmail.com>
2019-09-24 13:02:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,10 @@ end
 def summon_captain_planet(array)
   array.map do |call|
     "#{call.capitalize}!"
-end
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
 end
 
 def find_the_cheese# code an argument here
