9b61f7dcafae2c2834d277dae415964864361cc4
Anne Fausto <a.r.fausto@gmail.com>
2019-09-24 13:05:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,7 @@ def long_planeteer_calls(array)
   array.any?{|i| i.length > 4 }
 end
 
-def find_the_cheese
+def find_the_cheese(array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
